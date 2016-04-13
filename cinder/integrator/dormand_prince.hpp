c2eed4304dc38d5e3ecca314f20dd18da5a8bd0f
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-04-13 16:15:58 +0200

    Reduce default target error to account for smaller state vectors

--- cinder/integrator/dormand_prince.hpp
+++ cinder/integrator/dormand_prince.hpp
@@ -189,7 +189,7 @@ public:
 	 *
 	 * @param err is the target integration error.
 	 */
-	AdaptiveIntegratorBase(Real eTar = 0.1e-3) : invETar(1.0 / eTar)
+	AdaptiveIntegratorBase(Real eTar = 0.01e-6) : invETar(1.0 / eTar)
 	{
 		reset();
 	}
