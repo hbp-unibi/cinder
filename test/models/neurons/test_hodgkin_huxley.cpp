55a78df542db2bf157818e2df10976baff1eb048
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-04-30 00:57:36 +0200

    Add Hodgkin-Huxley unit test

--- test/models/neurons/test_hodgkin_huxley.cpp
+++ test/models/neurons/test_hodgkin_huxley.cpp
@@ -32,12 +32,18 @@
 namespace cinder {
 TEST(hodgkin_huxley, cond_exp)
 {
-	// Spike times computed using NEST simulation with 0.01 ms timestep
-	const std::vector<Time> expected_spikes{};
+	// Spike times computed using NEST simulation with 0.1 ms timestep; Spike at
+	// 209.2 is not emitted by the NEST simulation for unknown reasons
+	const std::vector<Time> expected_spikes{
+	    11.1_ms,  14.3_ms,  17.8_ms,  22._ms,   27.8_ms,  31.4_ms,  34.4_ms,
+	    37.7_ms,  40.9_ms,  43.6_ms,  46.5_ms,  49.7_ms,  52.2_ms,  54.9_ms,
+	    57.8_ms,  61.2_ms,  65.2_ms,  70.6_ms,  79.1_ms,  100.9_ms, 104._ms,
+	    107.5_ms, 111.7_ms, 117.2_ms, 126.4_ms, 201.1_ms, 203.4_ms, 205.5_ms,
+	    207.6_ms, 209.2_ms, 211.2_ms, 213.3_ms, 215.4_ms, 217.7_ms, 220.3_ms,
+	    223.1_ms, 226.3_ms, 230.1_ms, 234.9_ms, 241.9_ms, 257.3_ms};
 
 	DormandPrinceIntegrator integrator;
-	std::ofstream os("hh_trace.csv");
-	CSVRecorder recorder(os);
+	NullRecorder recorder;
 	NullController controller;
 	std::vector<Time> spikes;
 
@@ -45,20 +51,16 @@ TEST(hodgkin_huxley, cond_exp)
 	auto current_source = make_current_source(
 	    CondExp(0.1_uS, 10_ms, 0_V, {10_ms, 30_ms, 40_ms, 50_ms, 100_ms, 200_ms,
 	                                 202_ms, 204_ms, 206_ms, 208_ms}));
-	auto neuron = make_neuron<HodgkinHuxley>(current_source,
-	                                [&spikes](Time t) { spikes.push_back(t); });
+	auto neuron = make_neuron<HodgkinHuxley>(
+	    current_source, [&spikes](Time t) { spikes.push_back(t); });
 
 	// Solve the equation
 	make_solver(neuron, integrator, recorder, controller).solve(1_s);
 
-	for (size_t i = 0; i < spikes.size(); i++) {
-		std::cout << "Spike at " << spikes[i] << std::endl;
-	}
-
 	// Make sure the two results are within the range of one millisecond
-/*	ASSERT_EQ(expected_spikes.size(), spikes.size());
+	ASSERT_EQ(expected_spikes.size(), spikes.size());
 	for (size_t i = 0; i < spikes.size(); i++) {
-		EXPECT_GT(0.3_ms, std::abs(expected_spikes[i] - spikes[i]));
-	}*/
+		EXPECT_GT(0.5_ms, std::abs(expected_spikes[i] - spikes[i]));
+	}
 }
 }
---
+++ test/models/neurons/test_hodgkin_huxley_pynn.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import pyNN.nest as sim
+
+timestep=0.01
+sim.setup(timestep=timestep)
+
+source = sim.Population(1, sim.SpikeSourceArray,
+                        {"spike_times": [10, 30, 40, 50, 100, 200, 202, 204,
+                                         206, 208]})
+
+pop_cond_exp = sim.Population(1,
+                              sim.HH_cond_exp,
+                              {
+                                  'gbar_Na': 20.0,
+                                  'gbar_K': 6.0,
+                                  'g_leak': 0.01,
+                                  'cm': 0.2,
+                                  'v_offset': -63.0,
+                                  'e_rev_Na': 50.0,
+                                  'e_rev_K': -90.0,
+                                  'e_rev_leak': -65.0,
+                                  'e_rev_E': 0.0,
+                                  'e_rev_I': -80.0,
+                                  'tau_syn_E': 10.0,
+                                  'tau_syn_I': 2.0,
+                                  'i_offset': 0.0,
+                              })
+pop_cond_exp.record(["spikes", "v"])
+
+sim.Projection(source,
+               pop_cond_exp,
+               connector=sim.FromListConnector([(0, 0, 0.1, 0.1)]))
+
+sim.run(1000.0)
+
+print "Conductance based exponential HH neuron"
+print pop_cond_exp.get_data().segments[0].spiketrains
+
+voltage_trace = pop_cond_exp.get_data().segments[0].analogsignalarrays[0]
+n = len(voltage_trace);
+arr = np.zeros((n, 2));
+arr[:, 0] = np.linspace(0, timestep * (n - 1), n)
+arr[:, 1] = voltage_trace[:, 0]
+np.savetxt("hh_trace_pynn.csv", arr, delimiter=",")
+
+
+sim.end()
+
