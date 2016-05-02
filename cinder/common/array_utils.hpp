547e4078fcdb2167ef629b23606db421ffca9d07
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-05-02 17:19:53 +0200

    Formating

--- cinder/common/array_utils.hpp
+++ cinder/common/array_utils.hpp
@@ -41,9 +41,11 @@ namespace cinder {
 template <size_t... Is>
 struct seq {
 };
+
 template <size_t N, size_t... Is>
 struct gen_seq : gen_seq<N - 1, N - 1, Is...> {
 };
+
 template <size_t... Is>
 struct gen_seq<0, Is...> : seq<Is...> {
 };
